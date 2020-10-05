6ba8391982f34ab19abb988c693f06f2808d3809
David Lin <David.lin71@gmail.com>
2020-10-05 14:42:13 -0400

    finish rspec tests for caesar_cipher project

--- ruby_programming/test_driven_development/caesar_cipher_tdd/spec/caesar_cipher_spec.rb
+++ ruby_programming/test_driven_development/caesar_cipher_tdd/spec/caesar_cipher_spec.rb
@@ -1,7 +1,13 @@
 require './lib/caesar_cipher.rb'
 
-describe "#caesar_cipher" do
-  it "shifts the string" do
-    expect(caesar_cipher("What a string!", 5)).to eql('Bmfy f xywnsl!')
+describe '#caesar_cipher' do
+  it 'should take a string and shift all characters by an integer amount' do
+    expect(caesar_cipher("abcz", 5)).to eql('fghe')
+  end
+  it "should keep the same character case in the output" do
+    expect(caesar_cipher("HELLo", 8)).to eql('PMTTw')
+  end
+  it "should not change non-alphabetical characters" do
+    expect(caesar_cipher("!!!!", 4)).to eql('!!!!')
   end
 end
