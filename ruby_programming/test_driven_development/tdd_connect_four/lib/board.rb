a5719d1ab53f33023f688b12810b08a96ece0069
David Lin <David.lin71@gmail.com>
2020-10-21 20:43:00 -0400

    add methods and specs to check for game winning conditions

--- ruby_programming/test_driven_development/tdd_connect_four/lib/board.rb
+++ ruby_programming/test_driven_development/tdd_connect_four/lib/board.rb
@@ -19,25 +19,61 @@ class Board
     false
   end
 
+  def win_col?(mark)
+    6.downto(0).each do |col|
+      return true if 5.downto(2).all? { |row| @grid[row][col] == mark }
+      return true if 4.downto(1).all? { |row| @grid[row][col] == mark }
+      return true if 3.downto(0).all? { |row| @grid[row][col] == mark }
+    end
+    false
+  end
+
+  def win_row?(mark)
+    5.downto(0).each do |row|
+      return true if 6.downto(3).all? { |col| @grid[row][col] == mark }
+      return true if 5.downto(2).all? { |col| @grid[row][col] == mark }
+      return true if 4.downto(1).all? { |col| @grid[row][col] == mark }
+      return true if 3.downto(0).all? { |col| @grid[row][col] == mark }
+    end
+    false
+  end
+
+  def win_diagonal?(mark)
+    left_diagonals = false
+    right_diagonals = false
+    reversed = @grid.reverse
+
+    (3..5).each do |row|
+      (0..3).each { |col| left_diagonals = true if @grid[row][col] == mark && @grid[row-1][col+1] == mark && @grid[row-2][col+2] && @grid[row-3][col+3] == mark }
+    end
+
+    (3..5).each do |row|
+      (0..3).each { |col| left_diagonals = true if reversed[row][col] == mark && reversed[row-1][col+1] == mark && reversed[row-2][col+2] && reversed[row-3][col+3] == mark }
+    end
+
+    left_diagonals || right_diagonals
+  end
+
+
   def print
     puts "       1     2     3     4     5     6     7
     |     |     |     |     |     |     |     |
     |  #{@grid[0][0]}  |  #{@grid[0][1]}  |  #{@grid[0][2]}  |  #{@grid[0][3]}  |  #{@grid[0][4]}  |  #{@grid[0][5]}  |  #{@grid[0][6]}  |
     |_____|_____|_____|_____|_____|_____|_____|
     |     |     |     |     |     |     |     |
-    |  #{@grid[1][0]}  |  #{@grid[1][1]}  |  #{@grid[1][2]}  |  #{@grid[1][3]}  |  #{@grid[1][4]}  |  #{@grid[0][5]}  |  #{@grid[0][6]}  |
+    |  #{@grid[1][0]}  |  #{@grid[1][1]}  |  #{@grid[1][2]}  |  #{@grid[1][3]}  |  #{@grid[1][4]}  |  #{@grid[1][5]}  |  #{@grid[1][6]}  |
     |_____|_____|_____|_____|_____|_____|_____|
     |     |     |     |     |     |     |     |
-    |  #{@grid[2][0]}  |  #{@grid[2][1]}  |  #{@grid[2][2]}  |  #{@grid[2][3]}  |  #{@grid[2][4]}  |  #{@grid[0][5]}  |  #{@grid[0][6]}  |
+    |  #{@grid[2][0]}  |  #{@grid[2][1]}  |  #{@grid[2][2]}  |  #{@grid[2][3]}  |  #{@grid[2][4]}  |  #{@grid[2][5]}  |  #{@grid[2][6]}  |
     |_____|_____|_____|_____|_____|_____|_____|
     |     |     |     |     |     |     |     |
-    |  #{@grid[3][0]}  |  #{@grid[3][1]}  |  #{@grid[3][2]}  |  #{@grid[3][3]}  |  #{@grid[3][4]}  |  #{@grid[0][5]}  |  #{@grid[0][6]}  |
+    |  #{@grid[3][0]}  |  #{@grid[3][1]}  |  #{@grid[3][2]}  |  #{@grid[3][3]}  |  #{@grid[3][4]}  |  #{@grid[3][5]}  |  #{@grid[3][6]}  |
     |_____|_____|_____|_____|_____|_____|_____|
     |     |     |     |     |     |     |     |
-    |  #{@grid[4][0]}  |  #{@grid[4][1]}  |  #{@grid[4][2]}  |  #{@grid[4][3]}  |  #{@grid[4][4]}  |  #{@grid[0][5]}  |  #{@grid[0][6]}  |
+    |  #{@grid[4][0]}  |  #{@grid[4][1]}  |  #{@grid[4][2]}  |  #{@grid[4][3]}  |  #{@grid[4][4]}  |  #{@grid[4][5]}  |  #{@grid[4][6]}  |
     |_____|_____|_____|_____|_____|_____|_____|
     |     |     |     |     |     |     |     |
-    |  #{@grid[5][0]}  |  #{@grid[5][1]}  |  #{@grid[5][2]}  |  #{@grid[5][3]}  |  #{@grid[5][4]}  |  #{@grid[0][5]}  |  #{@grid[0][6]}  |
+    |  #{@grid[5][0]}  |  #{@grid[5][1]}  |  #{@grid[5][2]}  |  #{@grid[5][3]}  |  #{@grid[5][4]}  |  #{@grid[5][5]}  |  #{@grid[5][6]}  |
     |_____|_____|_____|_____|_____|_____|_____|"
   end
 
@@ -45,9 +81,7 @@ end
 
 
 =begin
-  
 symbols:
 ● 
 ○
-  
-end
+=end
--- ruby_programming/test_driven_development/tdd_connect_four/spec/board_spec.rb
+++ ruby_programming/test_driven_development/tdd_connect_four/spec/board_spec.rb
@@ -1,9 +1,11 @@
 require 'board'
 
 describe Board do
-  b = Board.new()
+  b = Board.new
+  g = Board.new
+  d = Board.new
 
-  describe "#initialize" do
+  describe ".initialize" do
     it "creates a 6 x 7 empty grid" do
       expect(b.grid).to be_a(Array)
       expect(b.grid.length).to eq(6)
@@ -21,7 +23,7 @@ describe Board do
   end
 
   describe "#empty_col?" do
-    b.place_mark(0, '○')
+    b.place_mark(0, '○')    
     b.place_mark(0, '○')
     b.place_mark(0, '○')
     b.place_mark(0, '○')
@@ -30,10 +32,53 @@ describe Board do
     it "returns true if the specified column is empty" do
       expect(b.empty_col?(1)).to be true
     end
-    it "returns false if the specified column is empty" do
+    it "returns false if the specified column is not empty" do
       expect(b.empty_col?(0)).to be false
     end
   end
+
+  describe "#win_col?" do
+    it "returns true if the specified player's mark has connected four in any col" do
+      expect(b.win_col?('○')).to be true
+    end
+    it "returns false if the specified player's mark has not connected four in any col" do
+      expect(b.win_col?('●')).to be false
+    end
+  end
+
+  describe "#win_row?" do
+    g.place_mark(0, '○')
+    g.place_mark(1, '○')
+    g.place_mark(2, '○')
+    g.place_mark(3, '○')
+    it "returns true if the specified player's mark has connected four in any row" do
+      expect(g.win_row?('○')).to be true
+    end
+    it "returns false if the specified player's mark has not connected four in any row" do
+      expect(g.win_row?('●')).to be false
+    end
+  end
+
+  describe "#win_diagonal?" do
+    d.place_mark(1, '●')
+    d.place_mark(2, '●')
+    d.place_mark(2, '●')
+    d.place_mark(3, '●')
+    d.place_mark(3, '●')
+    d.place_mark(3, '●')
+
+    d.place_mark(0, '○')
+    d.place_mark(1, '○')
+    d.place_mark(2, '○')
+    d.place_mark(3, '○')
+    it "returns true if the specified player's mark has connected four in any diagonal" do
+      expect(d.win_diagonal?('○')).to be true
+    end
+    it "returns false if the specified player's mark has not connected four in any diagonal" do
+      expect(d.win_diagonal?('●')).to be false
+    end
+  end
+
 =begin
   describe "#win?"
     it "returns false if a row, diagonal or column is filled with player mark" do
